12ab97bcf9cafe7eccfe9bb194b8cd9fd1af7b33
Joshen Lim <joshenlimek@gmail.com>
2022-02-24 16:37:55 +0800

    Fix SVG prop errors

--- studio/components/layouts/ProjectLayout/LayoutHeader/LayoutHeader.tsx
+++ studio/components/layouts/ProjectLayout/LayoutHeader/LayoutHeader.tsx
@@ -37,11 +37,11 @@ const LayoutHeader = ({ customHeaderComponents, breadcrumbs = [], headerBorder =
                     width="16"
                     height="16"
                     stroke="currentColor"
-                    stroke-width="1"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="1"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     fill="none"
-                    shape-rendering="geometricPrecision"
+                    shapeRendering="geometricPrecision"
                   >
                     <path d="M16 3.549L7.12 20.600"></path>
                   </svg>
